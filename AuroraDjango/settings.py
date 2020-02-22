a4e5244693b1b7998095a0d70c7dd8d5f249374c
IonizedElectric <IonizedElectric@users.github.com>
2020-02-22 17:03:37 +0000

    Enabled debugging

--- AuroraDjango/settings.py
+++ AuroraDjango/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#$5)77@)h58tnc9vnlr0tdjhz2qfkb+0nnfo$r)g#&tdynn_vd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
